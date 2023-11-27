b3fb784aa2776e746649a4d42baf54b14d7c5523
Imran Mustafa <imran.mustafa@ontariotechu.net>
2023-11-27 17:31:23 -0500

    basic model layout

--- prototype/src/views/HomeView.vue
+++ prototype/src/views/HomeView.vue
@@ -1,9 +1,112 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+  <div class="box">
+  <div class="columns">
+  <div class="column">
+  <figure class="image is-128x128">
+  <img src="https://bulma.io/images/placeholders/128x128.png">
+</figure>
+  </div>
+  <div class="column">
+  <div class="columns">
+  <div class="column">
+
+  <div class="field">
+  <label class="label">Pose</label>
+  <div class="select">
+  <select>
+    <option>Standing</option>
+    <option>Sitting</option>
+  </select>
+</div>
+</div>
+  </div>
+  <div class="column">
+
+  <div class="field">
+  <label class="label">Angle</label>
+  <div class="select">
+  <select>
+    <option>Front</option>
+    <option>Side</option>
+    <option>Back</option>
+  </select>
+</div>
+</div>
+  </div>
+  <div class="column">
+  <div class="field">
+  <label class="label">Location</label>
+  <div class="select">
+  <select>
+    <option>Select dropdown</option>
+    <option>With options</option>
+  </select>
+</div>
+</div>
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Outfit</label>
+  <div class="control">
+
+
+<div class="columns">
+  <div class="column">
+
+    <div class="field">
+  <div class="control">
+    <input type="image" src="https://bulma.io/images/placeholders/128x128.png" height=128 width=128>
+  </div>
+</div>
+    <div class="field">
+  <div class="control">
+    <input type="image" src="https://bulma.io/images/placeholders/128x128.png" height=128 width=128>
+  </div>
+</div>
+  </div>
+
+  <div class="column">
+
+    <div class="field">
+  <div class="control">
+    <input type="image" src="https://bulma.io/images/placeholders/128x128.png" height=128 width=128>
+  </div>
+</div>
+    <div class="field">
+  <div class="control">
+    <input type="image" src="https://bulma.io/images/placeholders/128x128.png" height=128 width=128>
+  </div>
+</div>
+  </div>
+
+  <div class="column">
+
+    <div class="field">
+  <div class="control">
+    <input type="image" src="https://bulma.io/images/placeholders/128x128.png" height=128 width=128>
+  </div>
+</div>
+    <div class="field">
+  <div class="control">
+    <input type="image" src="https://bulma.io/images/placeholders/128x128.png" height=128 width=128>
+  </div>
+</div>
+  </div>
+
+</div>
+
+
+  </div>
+</div>
+
+
+  </div>
+</div>
+  </div>
   </main>
 </template>
